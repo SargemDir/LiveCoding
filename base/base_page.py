b0e42a641b2fcc8f26ff69afd25e6dcb3a3d69dd
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2025-12-07 01:04:46 +0200

    modified and added pages

--- base/base_page.py
+++ base/base_page.py
@@ -8,8 +8,10 @@ class BasePage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10, poll_frequency=1)
 
+
     def open(self):
         self.driver.get(self.PAGE_URL)
+
 
     def is_opened(self):
         self.wait.until(EC.url_to_be(self.PAGE_URL))
